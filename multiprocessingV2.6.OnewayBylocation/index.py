3e06860b9406ac377591fbbd1c32bddecd38e18a
Amirsaj <109412094+Amirsaj@users.noreply.github.com>
2022-07-16 13:24:07 +0430

    Add files via upload

---
+++ multiprocessingV2.6.OnewayBylocation/index.py
@@ -0,0 +1,30 @@
+from multiprocessing import Process , Manager,Queue
+import multiprocessing as mp
+from threading import Thread
+import json
+import os
+import subprocess
+import cv2
+import numpy as np
+import tensorflow as tf
+import time
+# import status
+import ConFindercopy
+import pickle
+import ReadImage
+import PlateOrNot
+import Partioningcopy
+
+def index(jens,data,mod1=0,mod2=0,mod3=0):
+    if jens == "IMG":
+        pass
+        ConFindercopy.protot(data,mod1,mod2,mod3)
+    elif jens == "CON":
+        pass
+        PlateOrNot.PorNP(data,mod1,mod2,mod3)
+    elif jens == "TrustedCON":
+        pass
+        Partioningcopy.finncc(data,mod1,mod2,mod3)
+    elif jens == "Number":
+        print(time.time())
+        print("pelak",data)
